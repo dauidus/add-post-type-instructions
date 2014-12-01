df9217411fe8961cb31b0cab0297ff6dd5358c84
dauidus <dave@dauid.us>
2014-12-01 01:02:03 -0700

    update init file

--- default-content-editor-value.php
+++ default-content-editor-value.php
@@ -15,12 +15,12 @@
  * Plugin URI:        http://dauid.us
  * Description:       Allows admins to set default content to populate the editor for each active post type.
  * Version:           1.0.0
- * Author:            dauidite
+ * Author:            dauidus
  * Author URI:        http://dauid.us
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
- * GitHub Plugin URI: https://github.com/1fixdotio/custom-featured-image-metabox
+ * GitHub Plugin URI: https://github.com/dauidus/default-content-editor-value
  */
 
 // If this file is called directly, abort.
@@ -32,17 +32,17 @@ if ( ! defined( 'WPINC' ) ) {
  * Public-Facing Functionality
  *----------------------------------------------------------------------------*/
 
-require_once( plugin_dir_path( __FILE__ ) . 'public/class-custom-featured-image-metabox.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-default-content-editor-value.php' );
 
 /*
  * Register hooks that are fired when the plugin is activated or deactivated.
  * When the plugin is deleted, the uninstall.php file is loaded.
  *
  */
-register_activation_hook( __FILE__, array( 'Custom_Featured_Image_Metabox', 'activate' ) );
-register_deactivation_hook( __FILE__, array( 'Custom_Featured_Image_Metabox', 'deactivate' ) );
+register_activation_hook( __FILE__, array( 'Default_Content_Editor_Value', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Default_Content_Editor_Value', 'deactivate' ) );
 
-add_action( 'plugins_loaded', array( 'Custom_Featured_Image_Metabox', 'get_instance' ) );
+add_action( 'plugins_loaded', array( 'Default_Content_Editor_Value', 'get_instance' ) );
 
 /*----------------------------------------------------------------------------*
  * Dashboard and Administrative Functionality
@@ -54,84 +54,8 @@ add_action( 'plugins_loaded', array( 'Custom_Featured_Image_Metabox', 'get_insta
  */
 if ( is_admin() ) {
 
-	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-custom-featured-image-metabox-admin.php' );
-	add_action( 'plugins_loaded', array( 'Custom_Featured_Image_Metabox_Admin', 'get_instance' ) );
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-default-content-editor-value-admin.php' );
+	add_action( 'plugins_loaded', array( 'Default_Content_Editor_Value_Admin', 'get_instance' ) );
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// get custom post types with content editor support
-function supported_post_types() {
-
-	$post_types = get_post_types();
-	$results = array();
-
-	foreach ( $post_types as $pt ) {
-		if ( post_type_supports( $pt, 'editor' ) ) {
-			$results[] = $pt;
-		}
-	}
-	return $results;
-
-} // end supported_post_types
-
-
-
-foreach ( $post_types  as $post_type ) {
-
-	// value for editor_content
-	$post_type_edit_content = '';
-
-	// change content in editor
-	add_filter( 'the_editor_content', 'editor_content' ); 
-	function editor_content( $content ) {
-	    // Only return the filtered content if it's empty
-	    if ( empty( $content ) ) {
-	        $template  = '' . $post_type_edit_content . '';
-	        return $template;
-	    } else
-	        return $content;
-	}
-
-
-}
-
-
-
-
-
-
-
-
-
 
